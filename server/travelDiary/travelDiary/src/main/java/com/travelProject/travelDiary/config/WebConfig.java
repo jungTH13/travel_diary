3360b419401158025d31f471152651fb257f43f3
jungTH13 <87050915+jungTH13@users.noreply.github.com>
2023-03-06 23:24:11 +0900

    게스트 유저 토큰(JWT) 발행 기능 추가

--- server/travelDiary/travelDiary/src/main/java/com/travelProject/travelDiary/config/WebConfig.java
+++ server/travelDiary/travelDiary/src/main/java/com/travelProject/travelDiary/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.travelProject.travelDiary.config;
 
+import com.travelProject.travelDiary.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,8 +11,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST")
+                .allowCredentials(true)
                 .maxAge(3000);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor());
+
+        WebMvcConfigurer.super.addInterceptors(registry);
     }
 }
