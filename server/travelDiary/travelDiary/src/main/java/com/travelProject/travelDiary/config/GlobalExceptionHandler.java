e536333e24739d282dfdfbb8f2218b94150ac808
김경섭 <spfkem2@gmail.com>
2023-03-10 01:37:48 +0900

    소스 정리2 김경섭 2023-03-10

--- server/travelDiary/travelDiary/src/main/java/com/travelProject/travelDiary/config/GlobalExceptionHandler.java
+++ server/travelDiary/travelDiary/src/main/java/com/travelProject/travelDiary/config/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.travelProject.travelDiary.config;
 
-import com.travelProject.travelDiary.dto.ReturnBody;
+import com.travelProject.travelDiary.dto.ResponseBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,26 +10,20 @@ import static com.travelProject.travelDiary.dto.ErrorCode.INTERNAL_SERVER_ERROR;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler({ exceptionCode.class })
-    protected ReturnBody handleCustomException(exceptionCode ex) {
-        Long status = (long)ex.getErrorCode().getStatus();
+    protected ResponseBody handleCustomException(exceptionCode ex) {
+        int status = ex.getErrorCode().getStatus();
         String errorMessage = ex.getErrorCode().getMessage();
         String httpValue = String.valueOf(HttpStatus.valueOf(ex.getErrorCode().getStatus()));
 
-        ReturnBody returnBody = new ReturnBody();
-        returnBody.setCode(status);
-        returnBody.setMsg(errorMessage);
-        return returnBody;
+        return ResponseBody.builder().code(status).msg(errorMessage).results(httpValue).build();
     }
 
     @ExceptionHandler({ Exception.class })
-    protected ReturnBody handleServerException(Exception ex) {
-        Long status = (long) INTERNAL_SERVER_ERROR.getStatus();
+    protected ResponseBody handleServerException(Exception ex) {
+        int status = INTERNAL_SERVER_ERROR.getStatus();
         String errorMessage = INTERNAL_SERVER_ERROR.getMessage();
         String httpValue = String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR);
 
-        ReturnBody returnBody = new ReturnBody();
-        returnBody.setCode(status);
-        returnBody.setMsg(errorMessage);
-        return returnBody;
+        return ResponseBody.builder().code(status).msg(errorMessage).results(httpValue).build();
     }
 }
