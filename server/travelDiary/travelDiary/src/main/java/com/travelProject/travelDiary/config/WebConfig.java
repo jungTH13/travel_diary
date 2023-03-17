bcfe2444f1eebf1d8836ec74d46433d8332a625b
jungTH13 <87050915+jungTH13@users.noreply.github.com>
2023-03-17 19:23:59 +0900

    aws s3 연동

--- server/travelDiary/travelDiary/src/main/java/com/travelProject/travelDiary/config/WebConfig.java
+++ server/travelDiary/travelDiary/src/main/java/com/travelProject/travelDiary/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.travelProject.travelDiary.config;
 
 import com.travelProject.travelDiary.interceptor.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -8,11 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("")
-                .allowedOrigins("http://13.209.85.73:3000")
+                .allowedOrigins("http://13.209.85.73:3000","http://127.0.0.1:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3000);
@@ -20,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor());
+        registry.addInterceptor(this.authInterceptor);
 
         WebMvcConfigurer.super.addInterceptors(registry);
     }
