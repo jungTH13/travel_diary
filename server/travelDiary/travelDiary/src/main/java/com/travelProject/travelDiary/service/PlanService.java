19eb85ebea206dde081ce76bce8666729f53dba3
김경섭 <spfkem2@gmail.com>
2023-04-29 22:49:16 +0900

    유저 플랜리스트 조회시 가계부 총합과 체크여부 가져오는 로직 추가 김경섭 2023-04-29

--- server/travelDiary/travelDiary/src/main/java/com/travelProject/travelDiary/service/PlanService.java
+++ server/travelDiary/travelDiary/src/main/java/com/travelProject/travelDiary/service/PlanService.java
@@ -8,7 +8,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class PlanService {
@@ -29,6 +31,12 @@ public class PlanService {
     private PlanEtcRepository planEtcRepository;
 
     @Autowired
+    private PlanAccountBookRepository planAccountBookRepository;
+
+    @Autowired
+    private PlanCheckListTitleRepository planCheckListTitleRepository;
+
+    @Autowired
     private ModelMapper modelMapper;
 
     @SuppressWarnings("unchecked")
@@ -42,33 +50,64 @@ public class PlanService {
         List<PlanTransPort> transPortList = planTransPortRepository.findAllByTravel_IdAndUser_IdOrderByDepartDateAsc(travelId, userId);
         List<PlanEtc> etcList = planEtcRepository.findAllByTravel_IdAndUser_IdOrderByReservationDateAsc(travelId, userId);
 
+        List<PlanAccountBook> planAccountBookList = planAccountBookRepository.findAllByTravel_IdAndUser_Id(travelId, userId);
+        List<PlanCheckListTitle> planCheckListTitleList = planCheckListTitleRepository.findAllByTravel_IdAndUser_Id(travelId, userId);
+
         List<Map<String, Object>> planList = new ArrayList<>();
         for (PlanAirPlane planAirPlane : airPlaneList) {
             Map<String, Object> insertMap = planAirPlane.toMap();
+            setPlanAccountBookAndCheckList(insertMap, "pa", planAirPlane.getId(), planAccountBookList, planCheckListTitleList);
             planList.add(insertMap);
         }
 
         for (PlanHotel planHotel : hotelList) {
             Map<String, Object> insertMap = planHotel.toMap();
+            setPlanAccountBookAndCheckList(insertMap, "ph", planHotel.getId(), planAccountBookList, planCheckListTitleList);
             planList.add(insertMap);
         }
 
         for (PlanRestaurant planRestaurant : restaurantList) {
             Map<String, Object> insertMap = planRestaurant.toMap();
+            setPlanAccountBookAndCheckList(insertMap, "pr", planRestaurant.getId(), planAccountBookList, planCheckListTitleList);
             planList.add(insertMap);
         }
 
         for (PlanTransPort planTransPort : transPortList) {
             Map<String, Object> insertMap = planTransPort.toMap();
+            setPlanAccountBookAndCheckList(insertMap, "pt", planTransPort.getId(), planAccountBookList, planCheckListTitleList);
             planList.add(insertMap);
         }
 
         for (PlanEtc planEtc : etcList) {
             Map<String, Object> insertMap = planEtc.toMap();
+            setPlanAccountBookAndCheckList(insertMap, "pe", planEtc.getId(), planAccountBookList, planCheckListTitleList);
             planList.add(insertMap);
         }
 
         planList.sort(Comparator.comparing((Map<String, Object> map) -> (Date) map.get("orderDate")));
         return planList;
+    }
+
+    private void setPlanAccountBookAndCheckList(Map<String, Object> insertMapValue, String planType, Long planTypeId
+            , List<PlanAccountBook> bookList
+            , List<PlanCheckListTitle> checkList) {
+        List<PlanAccountBook> accountBookResult = bookList.stream()
+                .filter(title -> title.getPlanTypeId() == planTypeId && title.getPlanType().equals(planType))
+                .collect(Collectors.toList());
+
+        Optional<PlanCheckListTitle> checkResult = checkList.stream()
+                .filter(title -> title.getPlanTypeId() == planTypeId && title.getPlanType().equals(planType))
+                .findAny();
+
+        if (accountBookResult.size() > 0) {
+            BigDecimal sumAmount = accountBookResult.stream()
+                    .map(PlanAccountBook::getAmountOfPayment)
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+            insertMapValue.put("amountOfPayment", sumAmount);
+        }
+
+        if (checkResult.isPresent()) {
+            insertMapValue.put("isCompleted", checkResult.map(PlanCheckListTitle::getIsCompleted));
+        }
     }
 }
