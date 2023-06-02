3b65aba016805832aa77621bab52159056796a12
jungTH13 <87050915+jungTH13@users.noreply.github.com>
2023-06-02 15:07:34 +0900

    loggin test2

--- server/travelDiary/travelDiary/src/test/java/com/travelProject/travelDiary/TravelDiaryApplicationTests.java
+++ server/travelDiary/travelDiary/src/test/java/com/travelProject/travelDiary/TravelDiaryApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TravelDiaryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
